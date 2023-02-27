15370094c946be631e6600a544250f4b6318d70c
Santiago Restrepo <santiago.restrepop@perficient.com>
2023-02-27 00:27:28 -0500

    Improve exception handling for BindException
    - When more than 1 validation error occurs on DTO fields, an errorList will be shown into the Error Details object summarizing each error.

--- src/main/java/com/acelerati/management_service/application/dto/request/InventorySearchCriteriaDTO.java
+++ src/main/java/com/acelerati/management_service/application/dto/request/InventorySearchCriteriaDTO.java
@@ -11,10 +11,10 @@ import javax.validation.constraints.Min;
 @Getter
 public class InventorySearchCriteriaDTO {
 
-    @Min(0)
+    @Min(value = 0, message = "The From range of the filter must be at least 0")
     private final Long fromUnitPrice;
 
-    @Min(0)
+    @Min(value = 0, message = "The To range of the filter must be at least 0")
     private final Long toUnitPrice;
 
     private final String category;
