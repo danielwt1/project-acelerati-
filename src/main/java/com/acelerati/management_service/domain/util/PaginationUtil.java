c4df078bc3d691365c23250b29721ce562fea574
Santiago Restrepo <santiago.restrepop@perficient.com>
2023-04-13 02:26:18 -0500

    Update pagination class
    - Centralized the getPaginatedData logic and supported any type of arraylist as input.
    - Fixed pagination indicators as totalResults, firstResultIndex, etc.
    - Added PageOutOfBoundsException to handle the case when the user asks for too high page numbers.
    - Added some debug logging.

--- src/main/java/com/acelerati/management_service/domain/util/PaginationUtil.java
+++ src/main/java/com/acelerati/management_service/domain/util/PaginationUtil.java
@@ -1,24 +1,29 @@
 package com.acelerati.management_service.domain.util;
 
+import com.acelerati.management_service.domain.exception.PageOutOfBoundsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PaginationUtil {
-
-    public static final Integer DEFAULT_PAGE_SIZE = 20;
+    public static final Logger logger = LoggerFactory.getLogger(PaginationUtil.class);
+    public static final Long DEFAULT_PAGE_SIZE = 20L;
     public static final String NO_RECORDS_FOUND = "No records found";
 
-    private Integer pageSize;
-    private Integer pageNumber;
+    private Long pageSize;
+    private Long pageNumber;
     private String description;
-    private Integer firstResultIndex;
-    private Integer lastResultIndex;
+    private Long firstResultIndex;
+    private Long lastResultIndex;
     private Long totalResults;
 
     public PaginationUtil() {
         // This is used to prevent ambiguous constructors to Mapstructs
     }
-    public PaginationUtil(Integer pageSize, Integer pageNumber, String description, Integer firstResultIndex,
-                           Integer lastResultIndex, Long totalResults) {
+    public PaginationUtil(Long pageSize, Long pageNumber, String description, Long firstResultIndex,
+                           Long lastResultIndex, Long totalResults) {
         this.pageSize = pageSize;
         this.pageNumber = pageNumber;
         this.description = description;
@@ -27,24 +32,24 @@ public class PaginationUtil {
         this.totalResults = totalResults;
     }
 
-    public PaginationUtil(Integer pageSize, Integer pageNumber) {
+    public PaginationUtil(Long pageSize, Long pageNumber) {
         this.pageSize = pageSize;
         this.pageNumber = pageNumber;
     }
 
-    public Integer getPageSize() {
+    public Long getPageSize() {
         return pageSize;
     }
 
-    public void setPageSize(Integer pageSize) {
+    public void setPageSize(Long pageSize) {
         this.pageSize = pageSize;
     }
 
-    public Integer getPageNumber() {
+    public Long getPageNumber() {
         return pageNumber;
     }
 
-    public void setPageNumber(Integer pageNumber) {
+    public void setPageNumber(Long pageNumber) {
         this.pageNumber = pageNumber;
     }
 
@@ -69,32 +74,51 @@ public class PaginationUtil {
      * This method assumes that pageNumber and pageSize are already set for the pageable object.
      * @return the index which the database will use to start pulling the results.
      */
-    public Integer getFirstResultIndex() {
+    public Long getFirstResultIndex() {
         return firstResultIndex;
     }
 
-    public Integer getLastResultIndex() {
+    public Long getLastResultIndex() {
         return lastResultIndex;
     }
 
-    public void setLastResultIndex(Integer lastResultIndex) {
+    public void setLastResultIndex(Long lastResultIndex) {
         this.lastResultIndex = lastResultIndex;
     }
 
-    public Integer calculateOffset() {
-        return (pageNumber - 1) * pageSize;
+    public Long calculateOffset() {
+        return (pageNumber - 1L) * pageSize;
     }
 
-    public void updateAttributesFromListResults(List<?> results) {
+    public <T> void updateAttributesFromListResults(List<T> results) throws PageOutOfBoundsException {
         if (results.isEmpty()) {
-            description = NO_RECORDS_FOUND;
-            firstResultIndex = null;
-            lastResultIndex = null;
             totalResults = 0L;
+            description = NO_RECORDS_FOUND;
+            firstResultIndex = 0L;
+            lastResultIndex = 0L;
         } else {
+            totalResults = (long) results.size();
+            if (isPageOutOfBounds()) {
+                throw new PageOutOfBoundsException("This record set cannot be navigated any further");
+            }
             firstResultIndex = calculateOffset();
-            lastResultIndex = firstResultIndex + results.size() - 1;
+            lastResultIndex = Math.min(totalResults - 1, firstResultIndex + pageSize - 1);
             description = String.format("Showing %d to %d of %d results.", firstResultIndex + 1, lastResultIndex + 1, totalResults);
         }
+    }
+
+    private boolean isPageOutOfBounds() {
+        return calculateOffset() >= totalResults;
+    }
+
+    public <T> List<T> getPaginatedData(List<T> unpaginatedData) {
+        if (!unpaginatedData.isEmpty()) {
+            logger.debug("Applying pagination, skipping {} and limiting to {} records", firstResultIndex, lastResultIndex + 1 - firstResultIndex);
+            return unpaginatedData.stream()
+                    .skip(firstResultIndex)
+                    .limit(lastResultIndex + 1 - firstResultIndex)
+                    .collect(Collectors.toList());
+        }
+        return unpaginatedData;
     }
 }
