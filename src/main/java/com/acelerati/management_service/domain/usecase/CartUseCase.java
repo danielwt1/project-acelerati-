e0fae5be7d37929bdbc97dd17e1005537d6e469e
Santiago Restrepo <santiago.restrepop@perficient.com>
2023-05-03 20:03:19 -0500

    Resolve null reference when saving the cart items with arraylist.add(cartInventory)

--- src/main/java/com/acelerati/management_service/domain/usecase/CartUseCase.java
+++ src/main/java/com/acelerati/management_service/domain/usecase/CartUseCase.java
@@ -4,6 +4,7 @@ import com.acelerati.management_service.domain.api.CartServicePort;
 import com.acelerati.management_service.domain.model.CartInventoryModel;
 import com.acelerati.management_service.domain.model.CartModel;
 import com.acelerati.management_service.domain.model.InventoryModel;
+import com.acelerati.management_service.domain.spi.CartInventoryPersistencePort;
 import com.acelerati.management_service.domain.spi.CartPersistencePort;
 import com.acelerati.management_service.domain.spi.InventoryPersistencePort;
 import com.acelerati.management_service.domain.exception.ProductNotFoundException;
@@ -12,30 +13,39 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class CartUseCase implements CartServicePort {
-
     private static final String PRODUCT_NOT_FOUND_MESSAGE = "Product selected not found";
+
     private final CartPersistencePort cartPersistencePort;
     private final InventoryPersistencePort inventoryPersistencePort;
+    private final CartInventoryPersistencePort cartInventoryPersistencePort;
 
-    public CartUseCase(CartPersistencePort cartPersistencePort, InventoryPersistencePort inventoryPersistencePort) {
+    public CartUseCase(CartPersistencePort cartPersistencePort, InventoryPersistencePort inventoryPersistencePort,
+                       CartInventoryPersistencePort cartInventoryPersistencePort) {
         this.cartPersistencePort = cartPersistencePort;
         this.inventoryPersistencePort = inventoryPersistencePort;
+        this.cartInventoryPersistencePort = cartInventoryPersistencePort;
+    }
+
+    @Override
+    public CartModel getCartByUserId(Long idUser) {
+        return this.cartPersistencePort.getCart(idUser).orElseGet(CartModel::new);
     }
 
     @Override
     public void addProductToCart(Long idUser, Long idProduct, Long amount) {
-
         InventoryModel inventoryModel = inventoryPersistencePort.getElementById(idProduct)
                 .orElseThrow(()->new ProductNotFoundException(PRODUCT_NOT_FOUND_MESSAGE));
 
         Optional<CartModel> cartModel = cartPersistencePort.getCartByIdUser(idUser);
+        CartModel cart;
 
-        if(cartModel.isEmpty()){
+        if(cartModel.isEmpty()) {
             cartModel = Optional.of(new CartModel(null, idUser, LocalDateTime.now()));
+            cart = cartPersistencePort.createCart(cartModel.get()); // At this point the Cart ID should not be null
+        } else {
+            cart = cartModel.get();
         }
-
-        CartInventoryModel cartInventoryModel = new CartInventoryModel(null, cartModel.get(), inventoryModel, amount);
-        cartPersistencePort.addProductToCart(cartInventoryModel);
-
+        CartInventoryModel cartInventoryModel = new CartInventoryModel(null, cart, inventoryModel, amount);
+        cartInventoryPersistencePort.saveCartItem(cartInventoryModel);
     }
 }
