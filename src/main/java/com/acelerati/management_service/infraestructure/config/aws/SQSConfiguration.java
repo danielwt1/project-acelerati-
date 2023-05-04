ae2ec1042da28ec3c238b412f5618c0d4f3607d8
Santiago Restrepo <santiago.restrepop@perficient.com>
2023-05-04 18:26:35 -0500

    Add AWS dependency and basic configuration

---
+++ src/main/java/com/acelerati/management_service/infraestructure/config/aws/SQSConfiguration.java
@@ -0,0 +1,33 @@
+package com.acelerati.management_service.infraestructure.config.aws;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SQSConfiguration {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public QueueMessagingTemplate queueMessagingTemplate() {
+        return new QueueMessagingTemplate(amazonSQSAsync());
+    }
+
+    private AmazonSQSAsync amazonSQSAsync() {
+        return AmazonSQSAsyncClientBuilder
+                .standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                .build();
+    }
+}
