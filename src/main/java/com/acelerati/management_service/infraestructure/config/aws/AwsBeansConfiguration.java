486015f46aea6dc95f6204a6e86287f75eb57180
Daniel Ramos <dramosg@ucentral.edu.co>
2023-06-08 19:20:55 -0500

    first commit

--- src/main/java/com/acelerati/management_service/infraestructure/config/aws/AwsBeansConfiguration.java
+++ src/main/java/com/acelerati/management_service/infraestructure/config/aws/AwsBeansConfiguration.java
@@ -7,28 +7,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
+import software.amazon.awssdk.auth.credentials.ContainerCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.SystemPropertyCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 
 @Configuration
 public class AwsBeansConfiguration {
-    @Value("${aws.accessKeyId}")
-    private String accessKey;
-
-    @Value("${aws.secretKey}")
-    private String secretKey;
-
     @Value("${aws.region}")
     private String region;
 
     @Bean
     public SnsClient snsClient() {
-        AwsCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         Region awsRegion = Region.of(region);
         return SnsClient.builder()
                 .region(awsRegion)
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .credentialsProvider(this.getProviderChain())
                 .build();
     }
     @Bean
@@ -39,6 +39,16 @@ public class AwsBeansConfiguration {
     public NotificationPort getNotificatrion(){
         return new NotificationSnsAwsAdapter(snsClient(),getSnsTopicCreator() );
 
+    }
+    private AwsCredentialsProviderChain getProviderChain() {
+        return AwsCredentialsProviderChain.builder()
+                .addCredentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .addCredentialsProvider(SystemPropertyCredentialsProvider.create())
+                .addCredentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
+                .addCredentialsProvider(ProfileCredentialsProvider.create())
+                .addCredentialsProvider(ContainerCredentialsProvider.builder().build())
+                .addCredentialsProvider(InstanceProfileCredentialsProvider.create())
+                .build();
     }
 
 }
